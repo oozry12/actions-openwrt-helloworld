6aac7ac81d24d531a0d644a4af4507b64dd58720
244522645 <244522645@qq.com>
2022-03-08 17:20:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,11 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
+# git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-mv package/lean/OpenClash/luci-app-openclash package/lean/luci-app-openclash
-rm -rf package/lean/OpenClash
+# mv package/lean/OpenClash/luci-app-openclash package/lean/luci-app-openclash
+# rm -rf package/lean/OpenClash
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
 
